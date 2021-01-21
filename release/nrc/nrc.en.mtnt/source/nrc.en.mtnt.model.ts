4bcf51a756da48e4aad0e2677bffaf0cdb232d02
jahorton <jahorton@sil.org>
2021-01-21 08:24:18 +0700

    fix: mtnt detection and use of casing

--- release/nrc/nrc.en.mtnt/source/nrc.en.mtnt.model.ts
+++ release/nrc/nrc.en.mtnt/source/nrc.en.mtnt.model.ts
@@ -1,7 +1,8 @@
 const source: LexicalModelSource = {
   format: 'trie-1.0',
   wordBreaker: 'default',
-  sources: ['mtnt.tsv']
+  sources: ['mtnt.tsv'],
+  languageUsesCasing: true
 };
 
 export default source;
