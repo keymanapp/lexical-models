09c537ba5e5b7fef7052f84664ce1255cc48d6fe
MakaraSok <makara@keyman.com>
2021-04-21 15:50:05 +0700

    tweak the wordbreaker

--- release/sil/sil_international.km.nckw/source/sil_international.km.nckw.model.ts
+++ release/sil/sil_international.km.nckw/source/sil_international.km.nckw.model.ts
@@ -10,6 +10,17 @@ const source: LexicalModelSource = {
   format: 'trie-1.0',
   wordBreaker: 'default',
   sources: ['wordlist.tsv'],,
+    wordBreaker: function(str: string) {
+    return str.split(/\s|\u200b/).map(function(token) {
+      return {
+        left: str.indexOf(token),
+        start: str.indexOf(token),
+        right: str.indexOf(token) + token.length,
+        end: str.indexOf(token) + token.length,
+        text: token
+      }
+    });
+  },
   punctuation: {
     insertAfterWord: "\u200B"
   }
