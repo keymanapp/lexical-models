a5d476f89eba3f65ecffa312f6085b47305808ab
Jeff Heath <jeff_heath@sil.org>
2022-01-06 10:23:03 +0100

    [sil_tchad_dzg] Initial submission of Dazaga predictive keyboard

---
+++ release/sil/sil.dzg.lexique/source/sil.dzg.lexique.model.ts
@@ -0,0 +1,10 @@
+/*Dazaga data from Kevin Walters
+*/
+
+const source: LexicalModelSource = {
+  format: 'trie-1.0',
+  wordBreaker: 'default',
+  sources: ['wordlist.tsv', 'textwords.tsv'],
+  LanguageUsesCasing: true
+};
+export default source;
