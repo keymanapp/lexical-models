1ecd23022c0d47b7528a0780597d4a488a13914d
MakaraSok <makara@keyman.com>
2022-08-17 16:19:06 +0700

    add the temporary fix for ZWSP as word boundary

--- release/sil/sil.km.cnd/source/sil.km.cnd.model.ts
+++ release/sil/sil.km.cnd/source/sil.km.cnd.model.ts
@@ -8,9 +8,10 @@
 
 const source: LexicalModelSource = {
   format: 'trie-1.0',
-  sources: ['wordlist.tsv'],
-  wordBreaker: function(str: string) {
-    return str.split(/\s/).map(function(token) {
+  wordBreaker: 'default',
+  sources: ['wordlist.tsv'],,
+    wordBreaker: function(str: string) {
+    return str.split(/\s|\u200b/).map(function(token) {
       return {
         left: str.indexOf(token),
         start: str.indexOf(token),
@@ -19,6 +20,9 @@ const source: LexicalModelSource = {
         text: token
       }
     });
+  },
+  punctuation: {
+    insertAfterWord: "\u200B"
   }
 };
 export default source;
