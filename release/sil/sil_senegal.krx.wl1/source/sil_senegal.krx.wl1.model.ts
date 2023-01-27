758202f3690ce306b669b0bee0f987438b3918b7
madskinner2 <93286275+madskinner2@users.noreply.github.com>
2023-01-27 14:54:06 +0000

    Karon upload

---
+++ release/sil/sil_senegal.krx.wl1/source/sil_senegal.krx.wl1.model.ts
@@ -0,0 +1,14 @@
+/*
+  Predictive text model for Karon (krx, Senegal), based on a word list.
+  
+  This is a minimal lexical model source that uses a tab delimited wordlist.
+  See documentation online at https://help.keyman.com/developer/ for
+  additional parameters.
+*/
+
+const source: LexicalModelSource = {
+  format: 'trie-1.0',
+  wordBreaker: 'default',
+  sources: ['wordlist.tsv'],
+};
+export default source;
