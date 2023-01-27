0f0470a0f827795b90f1b3e800d996088034fd6e
madskinner2 <93286275+madskinner2@users.noreply.github.com>
2023-01-27 14:56:49 +0000

    Saafi-Saafi (Latin) upload

---
+++ release/sil/sil_senegal.sav.wl1/README.md
@@ -0,0 +1,20 @@
+wl1 lexical model
+===================
+
+Description
+-----------
+
+Predictive text model for Saafi-Saafi (sav, Senegal), based on a word list.
+
+Links
+-----
+
+Supported Platforms
+-------------------
+ * iPhone
+ * iPad
+ * Android phone
+ * Android tablet
+ * Mobile devices
+ * Tablet devices
+
---
+++ release/sil/sil_senegal.sav.wl1/source/sil_senegal.sav.wl1.model.ts
@@ -0,0 +1,13 @@
+/*Predictive text model for Saafi-Saafi (sav, Senegal), based on a word list.
+  
+  This is a minimal lexical model source that uses a tab delimited wordlist.
+  See documentation online at https://help.keyman.com/developer/ for
+  additional parameters.
+*/
+
+const source: LexicalModelSource = {
+  format: 'trie-1.0',
+  wordBreaker: 'default',
+  sources: ['wordlist.tsv'],
+};
+export default source;
