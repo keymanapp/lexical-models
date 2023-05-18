97bf821a323942160daa786c76a6aa5488277f59
madskinner2 <93286275+madskinner2@users.noreply.github.com>
2023-05-18 16:16:01 +0100

    Casing enabled
    Casing enabled, version 1.1.0

--- release/sil/sil_senegal.ndv.wl1/source/sil_senegal.ndv.wl1.model.ts
+++ release/sil/sil_senegal.ndv.wl1/source/sil_senegal.ndv.wl1.model.ts
@@ -9,6 +9,7 @@
 const source: LexicalModelSource = {
   format: 'trie-1.0',
   wordBreaker: 'default',
-  sources: ['wordlist.tsv'],
+  sources: ['wordlist.tsv'],,
+  languageUsesCasing: true
 };
 export default source;
