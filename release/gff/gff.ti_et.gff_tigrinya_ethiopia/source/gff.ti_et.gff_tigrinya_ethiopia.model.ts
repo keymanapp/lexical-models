8d5f59f9f31597e3d2efa0be915ebc2f8820f125
dyacob <yacob@geez.org>
2023-06-18 11:07:16 -0400

    Initial commit of an Ethiopian-Tigrinya lexical model.

---
+++ release/gff/gff.ti_et.gff_tigrinya_ethiopia/source/gff.ti_et.gff_tigrinya_ethiopia.model.ts
@@ -0,0 +1,11 @@
+/*
+  GFF Tigrinya-Ethiopian language lexical model.
+*/
+
+
+const source: LexicalModelSource = {
+  format: 'trie-1.0',
+  wordBreaker: 'default',
+  sources: ['TigrinayEtWordList.tsv'],
+};
+export default source;
