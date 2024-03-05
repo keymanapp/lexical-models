cf7da52f17979f67a69fbb754abf5ae51bd0a805
Joshua A. Horton <joshua_horton@sil.org>
2024-03-05 12:02:21 +0700

    change: [nrc.en.mtnt] word matching now ignores quotes, apostrophes

--- release/nrc/nrc.en.mtnt/source/nrc.en.mtnt.model.ts
+++ release/nrc/nrc.en.mtnt/source/nrc.en.mtnt.model.ts
@@ -1,6 +1,22 @@
 const source: LexicalModelSource = {
   format: 'trie-1.0',
   wordBreaker: 'default',
+  searchTermToKey: (wordform: string, applyCasing: CasingFunction) => {
+    // Refer to ${KEYMAN_ROOT}/developer/src/kmc-model/src/model-defaults.ts for
+    // the original version.
+    return Array.from(wordform
+          .normalize('NFKD')
+          // Remove any combining diacritics (if input is in NFKD)
+          .replace(/[\u0300-\u036F]/g, '')
+        ) // end of `Array.from`
+        .map(function(c) {
+          // Strip out all apostrophes and quotation marks; they are not distinct letters in English.
+          // Is of particular relevance for apostrophes due to English contractions; lazy typers
+          // often expect the apostrophe to be "filled in".
+          return applyCasing('lower', c).replace(/['"‘’“”]/, '')
+        })
+        .join('');
+  },
   sources: ['mtnt.tsv'],
   languageUsesCasing: true
 };
