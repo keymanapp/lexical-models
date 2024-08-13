83fcf1784bdd9abc68b3b44531f78725ce11e012
quwatsunny <48961603+quwatsunny@users.noreply.github.com>
2024-08-13 00:40:48 -0400

    New Lexical Model
    new burushaski lexical model for the perso arabic keyboard

---
+++ release/bsk_arab/HISTORY.md
@@ -0,0 +1,6 @@
+model Change History
+====================
+
+1.0 (2023-10-15)
+----------------
+* Burushaski BSK-Arab Lexical Model for Burushaski Keyboard, Created by Quwat K. Sunny
---
+++ release/bsk_arab/source/quwat.bsk-arab.model.model.ts
@@ -0,0 +1,35 @@
+/*Burushaski wordlist bsk-arab 1.0.2 */
+
+const source: LexicalModelSource = {
+  format: 'trie-1.0',
+  wordBreaker: 'default',
+  sources: ['wordlist.tsv'],
+  punctuation: {
+    quotesForKeepSuggestion: {
+       open: "«", close: "»",
+       isRTL: true
+    },
+  },
+  searchTermToKey: function (term) {
+    // This pattern removes common Arabic script combiing diacritics and the zero-width non-joiner.
+    // Except, skip the ARABIC_MADDAH_ABOVE. We want that to stay with its base character ALEF
+    const COMBINING_DIACRITICAL_MARKS = /[\u064b-\u0652\u0654-\u065f\u0670\u200C]/g;
+   
+    // Converts to Unicode Normalization form D.
+    // This means that MOST accents and diacritics have been "decomposed" and
+    // are stored as separate characters. We can then remove these separate
+    // characters!
+    // e.g., Å = A + °
+    let normalizedTerm = term.normalize('NFC');
+   
+    // Now, using the pattern above replace each diacritic with the
+    // empty string. This effectively removes all diacritics!
+    // e.g.,  a + ° = a
+    let termWithoutDiacritics = normalizedTerm.replace(COMBINING_DIACRITICAL_MARKS, '')
+   
+    // The resultant key is normalized and without diacritics
+    return termWithoutDiacritics;
+  },
+
+};
+export default source;
