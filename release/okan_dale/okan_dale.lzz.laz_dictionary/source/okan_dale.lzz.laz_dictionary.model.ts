acfd77bf1582a2b93c188543af7fab0c6987c01f
okandale <153693839+okandale@users.noreply.github.com>
2024-12-02 22:09:06 -0500

    Update okan_dale.lzz.laz_dictionary.model.ts

--- release/okan_dale/okan_dale.lzz.laz_dictionary/source/okan_dale.lzz.laz_dictionary.model.ts
+++ release/okan_dale/okan_dale.lzz.laz_dictionary/source/okan_dale.lzz.laz_dictionary.model.ts
@@ -1,5 +1,5 @@
 /*
-  Laz Dictionary 1.2 generated from template.
+  Laz Dictionary
   
   This is a minimal lexical model source that uses a tab delimited wordlist.
   See documentation online at https://help.keyman.com/developer/ for
