f75852ff9ba08d8ef3bec420743b0745b799487a
gushmazuko <gushmazuko@protonmail.com>
2025-01-21 17:42:49 +0100

    add lexical model for chechen latin

---
+++ experimental/chechen_latin/source/chechen_language.ce-latn.chechen.model.ts
@@ -0,0 +1,19 @@
+/*
+  Chechen 1.0
+
+  Word forms are sourced from the corpus at corpora.dosham.info
+*/
+
+const source: LexicalModelSource = {
+  format: 'trie-1.0',
+  wordBreaker: 'default',
+  sources: ['wordlist.tsv'],,
+  languageUsesCasing: true,
+  punctuation: {
+    quotesForKeepSuggestion: {
+      open: "\"",
+      close: "\""
+    }
+  }
+};
+export default source;
