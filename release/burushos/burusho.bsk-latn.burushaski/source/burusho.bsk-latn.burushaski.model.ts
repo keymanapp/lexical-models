91f742173ab9ce215bacf72a8d59bf015a2534a4
quwatsunny <48961603+quwatsunny@users.noreply.github.com>
2025-01-24 19:16:40 -0500

    minor changes

--- release/burushos/burusho.bsk-latn.burushaski/source/burusho.bsk-latn.burushaski.model.ts
+++ release/burushos/burusho.bsk-latn.burushaski/source/burusho.bsk-latn.burushaski.model.ts
@@ -1,10 +1,7 @@
 /*
   Burushaski 1.0
 
-  This is a minimal lexical model source that uses a tab delimited wordlist.
-  See documentation online at https://help.keyman.com/developer/ for
-  additional parameters.
-*/
+  This lexical model is for Predictive Writing in the Burushaski Language latin script. */
 
 const source: LexicalModelSource = {
   format: 'trie-1.0',
