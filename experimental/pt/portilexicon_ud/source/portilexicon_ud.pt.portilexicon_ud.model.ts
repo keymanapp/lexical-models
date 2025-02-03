1bbba6bb82c34fb2b5ac3084fcef5c9eec8fe8ed
nm <fhdfha@hhb.com>
2025-02-03 15:29:09 +0000

    add PortiLexicon-UD

---
+++ experimental/pt/portilexicon_ud/source/portilexicon_ud.pt.portilexicon_ud.model.ts
@@ -0,0 +1,14 @@
+/*
+  PortiLexicon-UD 1.0
+
+  This is a minimal lexical model source that uses a tab delimited wordlist.
+  See documentation online at https://help.keyman.com/developer/ for
+  additional parameters.
+*/
+
+const source: LexicalModelSource = {
+  format: 'trie-1.0',
+  wordBreaker: 'default',
+  sources: ['wordlist.tsv'],
+};
+export default source;
