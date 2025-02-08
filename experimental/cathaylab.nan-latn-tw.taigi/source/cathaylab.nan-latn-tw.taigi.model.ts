407ca8c6357f3ef6e056be8202de9021b143b7ef
cathaylab <142644166+Cathaylaboratory@users.noreply.github.com>
2025-02-08 07:39:10 +0000

    Add files via upload

---
+++ experimental/cathaylab.nan-latn-tw.taigi/source/cathaylab.nan-latn-tw.taigi.model.ts
@@ -0,0 +1,20 @@
+
+const source: LexicalModelSource = {
+  format: 'trie-1.0',
+  wordBreaker: {
+    use: 'default',     // we want to use the default word breaker, BUT!
+    // CUSTOMIZE THIS:
+    joinWordsAt: ['-'], // join words that contain hyphens
+  }
+  sources: ['wordlist.tsv'],
+    punctuation: {
+    insertAfterWord: ""
+  }
+  languageUsesCasing: true,
+  searchTermToKey: function(term, applyCasing) {
+    return Array.from(term)
+      .map(function(c) { return applyCasing('lower', c) };
+  }
+};
+
+export default source;
